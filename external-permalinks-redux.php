f9a55e63cd131aa21b621b0d32122d974f95950c
ethitter <ethitter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 17:28:59 +0000

    Version 1.0.4
    Convert to traditional singleton, clean up PHPDoc.
    See https://github.com/ethitter/External-Permalinks-Redux/
    
    git-svn-id: https://plugins.svn.wordpress.org/external-permalinks-redux/trunk@562545 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- external-permalinks-redux.php
+++ external-permalinks-redux.php
@@ -3,7 +3,7 @@
 Plugin Name: External Permalinks Redux
 Plugin URI: http://www.thinkoomph.com/plugins-modules/external-permalinks-redux/
 Description: Allows users to point WordPress objects (posts, pages, custom post types) to a URL of your choosing. Inspired by and backwards-compatible with <a href="http://txfx.net/wordpress-plugins/page-links-to/">Page Links To</a> by Mark Jaquith. Written for use on WordPress.com VIP.
-Version: 1.0.3
+Version: 1.0.4
 Author: Erick Hitter (Oomph, Inc.)
 Author URI: http://www.thinkoomph.com/
 
@@ -23,15 +23,30 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class external_permalinks_redux {
-	/*
+	/**
 	 * Class variables
 	 */
+	protected static $instance;
+
 	var $meta_key_target = '_links_to';
 	var $meta_key_type = '_links_to_type';
 	var $status_codes;
 
-	/*
+	/**
+	 * Instantiate class as a singleton
+	 *
+	 * @return object
+	 */
+	static function get_instance() {
+		if ( ! isset( self::$instance ) )
+			self::$instance = new external_permalinks_redux;
+
+		return self::$instance;
+	}
+
+	/**
 	 * Register actions and filters
+	 *
 	 * @uses add_action, add_filter
 	 * @return null
 	 */
@@ -64,8 +79,9 @@ class external_permalinks_redux {
 		$this->status_codes = apply_filters( 'epr_status_codes', $status_codes );
 	}
 
-	/*
+	/**
 	 * Add meta box
+	 *
 	 * @uses apply_filters, add_meta_box
 	 * @action admin_init
 	 * @return null
@@ -81,8 +97,9 @@ class external_permalinks_redux {
 	}
 
 
-	/*
+	/**
 	 * Render meta box
+	 *
 	 * @param object $post
 	 * @uses _e, esc_url, get_post_meta, selected, wp_create_nonce
 	 * @return string
@@ -115,8 +132,9 @@ class external_permalinks_redux {
 	<?php
 	}
 
-	/*
+	/**
 	 * Save meta box input
+	 *
 	 * @param int $post_id
 	 * @uses wp_verify_nonce, esc_url_raw, update_post_meta, delete_post_meta
 	 * @action save_post
@@ -142,8 +160,9 @@ class external_permalinks_redux {
 		}
 	}
 
-	/*
+	/**
 	 * Filter post and custom post type permalinks
+	 *
 	 * @param string $permalink
 	 * @param object $post
 	 * @uses get_post_meta
@@ -157,8 +176,9 @@ class external_permalinks_redux {
 		return $permalink;
 	}
 
-	/*
+	/**
 	 * Filter page permalinks
+	 *
 	 * @param string $link
 	 * @param int $id
 	 * @uses get_post_meta
@@ -172,8 +192,9 @@ class external_permalinks_redux {
 		return $link;
 	}
 
-	/*
+	/**
 	 * Redirect to external link if object requested directly.
+	 *
 	 * @uses get_post_meta, wp_redirect
 	 * @action pre_get_posts
 	 * @return null
@@ -191,22 +212,19 @@ class external_permalinks_redux {
 		}
 	}
 }
-global $external_permalinks_redux;
-if( !is_a( $external_permalinks_redux, 'external_permalinks_redux' ) )
-	$external_permalinks_redux = new external_permalinks_redux;
 
-/*
+// Initialize the plugin if it hasn't already
+external_permalinks_redux::get_instance();
+
+/**
  * Wrapper for meta box function
  * Can be used as an alternative to the epr_post_types filter found in the plugin classes's action_admin_init function.
+ *
  * @param object $post
  * @uses $external_permalinks_redux
  * @return string
  */
 function external_permalinks_redux_meta_box( $post ) {
-	global $external_permalinks_redux;
-	if( !is_a( $external_permalinks_redux, 'external_permalinks_redux' ) )
-		$external_permalinks_redux = new external_permalinks_redux;
-
-	$external_permalinks_redux->meta_box( $post );
+	external_permalinks_redux::get_instance()->meta_box( $post );
 }
 ?>
